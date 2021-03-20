2b188f47aeae9c7117a6898546216287b3969511
haileemiu <39972604+haileemiu@users.noreply.github.com>
2021-03-20 09:16:46 -0500

    custom content based on auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,14 @@
                         </g>
                     </svg>
                 </div>
-
+                <div class="title m-b-md">
+                    @auth
+                        Hi, {{ Auth::user()->name }}
+                    @else
+                        Hello!
+                    @endauth
+                </div>
+                </div>
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
